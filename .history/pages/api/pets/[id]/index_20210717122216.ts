26b91559f804e1f645be7bfbda865f5cd38274da
maojunfeng <1009935357@qq.com>
2021-07-19 19:30:46 +0800

    deletehistory

--- .history/pages/api/pets/[id]/index_20210717122216.ts
+++
@@ -1,6 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import dbconnect from "../../../../utils/dbconnect";
-
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    await dbconnect
-}
